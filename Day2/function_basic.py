aaa55f511b64aa0cf548446634aa20d5ff3ef60e
songbae <overwatch94@naver.com>
2021-01-21 13:52:13 +0900

    day4 review python grammer

---
+++ Day2/function_basic.py
@@ -0,0 +1,23 @@
+def calculate(x,y):
+    return x*y
+ 
+def f(x):
+    return 2*x+7
+
+def g(x):
+    return x**2
+
+# x= 10
+# y=20
+# print('사각형의 y 길이:', y)
+# print(' 사각형의 길이:', x)
+
+
+# print('사각형의 넓이', calculate(x,y))
+x=2
+print(f(x)+g(x)+f(g(x))+g(f(x)))
+som= input()
+print(som)
+
+#print formatting
+#  
---
+++ Day3/code.py
@@ -0,0 +1,12 @@
+country_code={}
+country_code={"america":1, "korea":82,"china":86,"japan":81}
+for items in country_code.items():
+    print(items)
+print(country_code.keys())
+country_code['germman']=49
+print(country_code.values())
+
+for k,v in country_code.items():
+    print(k)
+    print(v)
+    
